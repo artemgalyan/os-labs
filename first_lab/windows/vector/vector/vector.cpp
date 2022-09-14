205bfe6ae2ae013ad9eeae93b35152d35a0e6ef9
artem.galyan <fpm.galyanAA@bsu.by>
2022-09-14 12:05:17 +0300

    fixed codestyle

--- first_lab/windows/vector/vector/vector.cpp
+++ first_lab/windows/vector/vector/vector.cpp
@@ -6,40 +6,40 @@
 
 namespace vector {
 
-  const Vector ZERO_ZERO = Vector(number::ZERO, number::ZERO);
-  const Vector ONE_ONE = Vector(number::ONE, number::ONE);
+const Vector ZERO_ZERO = Vector(number::ZERO, number::ZERO);
+const Vector ONE_ONE = Vector(number::ONE, number::ONE);
 
-  Vector::Vector(Number x, Number y) : x_(x), y_(y) {}
+Vector::Vector(Number x, Number y) : x_(x), y_(y) {}
 
-  double Vector::GetAngle() const {
-    double angle = atan((double)y_ / x_);
-    if (x_ >= number::ZERO) {
-      return angle;
-    }
-    if (y_ >= number::ZERO) {
-      return std::numbers::pi - angle;
-    }
-    return std::numbers::pi + angle;
+double Vector::GetAngle() const {
+  double angle = atan((double) y_ / x_);
+  if (x_ >= number::ZERO) {
+    return angle;
   }
-
-  double Vector::GetRadius() const {
-    return sqrt((int)x_ * x_ + y_ * y_);
+  if (y_ >= number::ZERO) {
+    return std::numbers::pi - angle;
   }
+  return std::numbers::pi + angle;
+}
 
-  number::Number Vector::GetX() const {
-    return x_;
-  }
+double Vector::GetRadius() const {
+  return sqrt((int) x_ * x_ + y_ * y_);
+}
 
-  number::Number Vector::GetY() const {
-    return y_;
-  }
+number::Number Vector::GetX() const {
+  return x_;
+}
 
-  Vector Vector::operator+(const Vector& v) const {
-    return Vector(x_ + v.x_, y_ + v.y_);
-  }
+number::Number Vector::GetY() const {
+  return y_;
+}
 
-  Vector Vector::operator-() const {
-    return Vector(-x_, -y_);
-  }
+Vector Vector::operator+(const Vector& v) const {
+  return Vector(x_ + v.x_, y_ + v.y_);
+}
+
+Vector Vector::operator-() const {
+  return Vector(-x_, -y_);
+}
 
 }
