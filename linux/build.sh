3a7be27f2478bf55044b710ba878c54532b49c46
artem.galyan <fpm.galyanAA@bsu.by>
2022-09-26 12:47:02 +0300

    update build.sh

--- linux/build.sh
+++ linux/build.sh
@@ -15,15 +15,13 @@ ar rvs $TEMP_DIR/libnumber.a $TEMP_DIR/libnumber.o
 echo ">>>> libnumber.a is built"
 
 c++ -c $VECTOR_DIR/vector.cpp $INCLUDE_NUMBER_HEADERS -fpic -std=$CPP_STD -o $TEMP_DIR/libvector.o
-c++ -shared $TEMP_DIR/libvector.o $TEMP_DIR/libnumber.a -o $TEMP_DIR/libvector.so
+c++ -shared $TEMP_DIR/libvector.o $TEMP_DIR/libnumber.a -o $BUILD_DIR/libvector.so
 
 echo ">>>> libvector.so is built"
 
 c++ -c $DEMO_DIR/main.cpp $INCLUDE_NUMBER_HEADERS $INCLUDE_VECTOR_HEADERS -std=$CPP_STD -o $TEMP_DIR/demo_proj.o
 
-export LD_PRELOAD=$TEMP_DIR/libvector.so
-
-c++ -o $BUILD_DIR/demo_proj $TEMP_DIR/demo_proj.o $TEMP_DIR/libnumber.a $TEMP_DIR/libvector.so
+c++ -o $BUILD_DIR/demo_proj $TEMP_DIR/demo_proj.o $TEMP_DIR/libnumber.a $BUILD_DIR/libvector.so
 
 echo ">>>> running demo_proj"
 ./$BUILD_DIR/demo_proj
