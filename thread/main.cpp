d9ade6f2c84d047c1f252aa9c32ea0b2e680de41
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-09-30 22:17:00 +0300

    changed tests

--- thread/main.cpp
+++ thread/main.cpp
@@ -2,12 +2,11 @@
 
 #include "matrix.h"
 #include "testing/MatrixTester.h"
-#include "matrix_utils.h"
 
 int main() {
-  const int m = 20;
-  const int n = 40;
-  const int l = 20;
+  const int m = 18;
+  const int n = 15;
+  const int l = 40;
   MatrixTester tester(m, n, l);
   tester.RunTests();
   return 0;
