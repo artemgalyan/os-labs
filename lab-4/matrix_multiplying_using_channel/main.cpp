3d3012a53691aa94e6dd660b56e2d2d34a805bc4
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-10-25 19:16:20 +0300

    tests for number of threads

--- lab-4/matrix_multiplying_using_channel/main.cpp
+++ lab-4/matrix_multiplying_using_channel/main.cpp
@@ -4,15 +4,14 @@
 #include "testing/MatrixTester.h"
 
 int main() {
-  const int size = 1000;
+  const int size = 500;
 
   const int m = size;
   const int n = size;
   const int l = size;
-  MatrixTester tester(m, n, l);
-  tester.SetStartBlockSize(490)
-        .SetEndBlockSize(510)
-        .CheckResults(true)
+  MatrixTester tester(m, n, l, ceil(size / 3.0));
+  tester.SetStartThreadsCount(18)
+        .CheckResults(false)
         .RunTests();
   return 0;
 }
--- lab-4/matrix_multiplying_using_channel/testing/MatrixTester.cpp
+++ lab-4/matrix_multiplying_using_channel/testing/MatrixTester.cpp
@@ -4,35 +4,34 @@
 
 #include <iomanip>
 
-MatrixTester::MatrixTester(int m, int n, int l) {
+MatrixTester::MatrixTester(int m, int n, int l, int block_size) : block_size_(block_size) {
   a_ = matrix::GenerateMatrix(m, n, MIN_MATRIX_VALUE, MAX_MATRIX_VALUE);
   b_ = matrix::GenerateMatrix(n, l, MIN_MATRIX_VALUE, MAX_MATRIX_VALUE);
-  max_block_size_ = std::min(m, std::min(n, l)) + 1;
-  end_block_size_ = max_block_size_;
 }
 
 void MatrixTester::RunTests() const {
   std::cout << "Starting tests.." << std::endl;
   std::vector<TestResult> results;
   Timer timer;
-  std::cout << "Running test for 1 thread" << std::endl;
-  timer.Reset();
-  auto result = a_ * b_;
-  auto duration = timer.MeasureTime();
-  for (int block_size = start_block_size_; block_size < max_block_size_ && block_size < end_block_size_;
-       block_size += step_) {
-    std::cout << "Running test for block size " << block_size << std::endl;
+  std::cout << "Initializing..." << std::endl;
+  def_matrix result;
+  if (check_results_)
+    result = a_ * b_;
+  int max_number_of_threads = ceil((double) a_.GetM() / block_size_) * ceil((double) a_.GetN() / block_size_)
+      * ceil((double) b_.GetN() / block_size_) * ceil((double) b_.GetM() / block_size_);
+  for (int threads = start_threads_count_; threads <= max_number_of_threads; threads += step_) {
+    std::cout << "Running test for " << threads << " thread(s). " << std::endl;
     TestResult tr;
-    tr.block_size = block_size;
+    tr.thread_count = threads;
     timer.Reset();
-    auto c = matrix::MultiplyMultithreaded(a_, b_, block_size);
+    auto c = matrix::MultiplyMultithreaded(a_, b_, block_size_);
     tr.duration = timer.MeasureTime();
     if (check_results_ && c != result) {
-      std::cerr << "An error occurred while multiplying matrices with block size " << block_size << std::endl;
+      std::cerr << "An error occurred while multiplying matrices with " << threads << " threads" << std::endl;
     }
     results.push_back(tr);
   }
-  PrintResults(results, {duration, -1});
+  PrintResults(results);
 }
 
 struct separate_thousands : std::numpunct<char> {
@@ -40,7 +39,7 @@ struct separate_thousands : std::numpunct<char> {
   string_type do_grouping() const override { return "\3"; }
 };
 
-void MatrixTester::PrintResults(const std::vector<TestResult>& res, TestResult one_threaded) const {
+void MatrixTester::PrintResults(const std::vector<TestResult>& res) const {
   std::cout << "Test for m = " << a_.GetM() << ", n = " << a_.GetN() << ", l = " << b_.GetN() << std::endl;
   auto fastest = *std::min_element(res.begin(), res.end(), [](const TestResult& a, const TestResult& b) {
     return a.duration < b.duration;
@@ -50,36 +49,26 @@ void MatrixTester::PrintResults(const std::vector<TestResult>& res, TestResult o
   std::cout << std::setw(15) << "description" << " | " << std::setw(15) << "time (microseconds)" << " | "
             << "% to 1 threaded"
             << std::endl;
-  std::cout << std::setw(15) << "1 threaded" << " | " << std::setw(15) << one_threaded.duration << " | " << "100%"
-            << std::endl;
   for (const auto& r : res) {
-    std::string info_string = std::to_string(r.block_size) + " block size";
-    std::cout << std::setw(15) << info_string << " | " << std::setw(15) << r.duration << " | "
-              << (double) r.duration / one_threaded.duration * 100 << '%';
+    std::string info_string = std::to_string(r.thread_count) + " threads";
+    std::cout << std::setw(15) << info_string << " | " << std::setw(15) << r.duration;
     if (r.duration == fastest.duration) {
       std::cout << "\t<-- fastest";
     }
     std::cout << std::endl;
   }
-  std::cout << "Fastest is: block size is " << fastest.block_size << ", time is " << fastest.duration << " ("
-            << (double) fastest.duration / one_threaded.duration * 100 << "%) from 1 threaded." << std::endl;
+  std::cout << "Fastest is: " << fastest.thread_count << " threads, time is " << fastest.duration << std::endl;
 }
-MatrixTester& MatrixTester::SetStartBlockSize(int value) {
-  start_block_size_ = value;
+
+MatrixTester& MatrixTester::CheckResults(bool value) {
+  check_results_ = value;
   return *this;
 }
 MatrixTester& MatrixTester::SetStep(int new_step) {
-  if (new_step < 1)
-    throw std::logic_error("Step should be >= 1");
   step_ = new_step;
   return *this;
 }
-
-MatrixTester& MatrixTester::SetEndBlockSize(int new_end_size) {
-  end_block_size_ = new_end_size;
-  return *this;
-}
-MatrixTester& MatrixTester::CheckResults(bool value) {
-  check_results_ = value;
+MatrixTester& MatrixTester::SetStartThreadsCount(int count) {
+  start_threads_count_ = count;
   return *this;
 }
